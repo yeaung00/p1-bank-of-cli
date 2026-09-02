3a77c3bdd12960fa3dac05dc883c4360ccf39ad5
YousefAbuadas <sparkburner@gmail.com>
2026-09-02 16:07:55 -0500

    implemented rough draft login method; added basic Business class declaration

--- bank/src/com/revature/API.java
+++ bank/src/com/revature/API.java
@@ -2,17 +2,10 @@ package com.revature;
 
 import java.util.*;
 
+//TODO: maybe only run() should be public and everthing else private?
 public class API {
-    // Attributes
     public Scanner s;
 
-    // Constructors
-
-    //////////////
-    // Methods ///
-    //////////////
-    
-    // Connor
     public void launch() {
         System.out.println("Welcome to Bank of CLI!");
         while (true) {
@@ -22,7 +15,6 @@ public class API {
             } else if (r) {
                 // Register
             } else if (q) {
-                // Quit loop
                 break;
             } else {
                 // Print nice error message and restart
@@ -30,47 +22,41 @@ public class API {
         }
     }
 
-    // After you register, it should send you back to the launch to login
-    // Ydur
     public void register() {
-
     }
 
-    // Yousef
-    public void login() {
-
+    //class is private because method should only be accessed within class (API) and not outside
+    private void login(String AcountID, String PIN) {
+        try {
+            if(Business.verifyCredentials(AcountID, PIN)) {
+                System.out.println("Login Successful!");
+            }
+            else {
+                System.out.println("Username or password is incorrect, please try again");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
     }
 
-    // This will be the query loop where it will ask you what you want to do:
-    // view balance, deposit, withdraw, transfer, or view activity
-    // Damon
     public void homeAccountPage() {
-        // Another query loop with those 5 tasks
     }
 
-
-    // First come first serve for these 5
     public void viewBalance() {
-
     }
 
     public void deposit() {
-
     }
 
     public void withdraw() {
-
     }
 
     public void transfer() {
-
     }
 
     public void viewActivity() {
-
     }
 
-    // Main
     public void run() {
         s = new Scanner(System.in);
         this.launch();
---
+++ bank/src/com/revature/Business.java
@@ -0,0 +1,7 @@
+package com.revature;
+
+public class Business {
+    public static boolean verifyCredentials(String AcountID, String PIN) {
+        return true;
+    }
+}
