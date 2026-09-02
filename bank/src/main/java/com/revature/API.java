0367da91719e49e23ca6370dada300b303c013df
connorg2404 <connortownson@gmail.com>
2026-09-02 15:32:16 -0400

    API Class Created

---
+++ bank/src/main/java/com/revature/API.java
@@ -0,0 +1,74 @@
+package com.revature;
+
+import java.util.*;
+
+public class API {
+    // Attributes
+    public Scanner s;
+
+    // Constructors
+    public API() {
+        s = new Scanner(System.in);
+    }
+
+    //////////////
+    // Methods ///
+    //////////////
+    public void launch() {
+        System.out.println("Welcome to Bank of CLI!");
+        while (true) {
+            System.out.println("To login, click l. To register, click r");
+            if (l) {
+                // Login
+            } else if (r) {
+                // Register
+            } else if (q) {
+                // Quit app
+            } else {
+                // Print nice error message and restart
+            }
+        }
+    }
+
+    // After you register, it should send you back to the launch to login
+    public void register() {
+
+    }
+
+    //
+    public void login() {
+
+    }
+
+    // This will be the query loop where it will ask you what you want to do:
+    // view balance, deposit, withdraw, transfer, or view activity
+    public void homeAccountPage() {
+
+    }
+
+    public void viewBalance() {
+
+    }
+
+    public void deposit() {
+
+    }
+
+    public void withdraw() {
+
+    }
+
+    public void transfer() {
+
+    }
+
+    public void viewActivity() {
+
+    }
+
+    // Main
+    public void run() {
+        this.launch();
+        s.close();
+    }
+}
