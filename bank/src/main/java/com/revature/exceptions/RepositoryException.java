e5ad5776a8a336e910e9a4a009b2a5b054188319
YousefAbuadas <sparkburner@gmail.com>
2026-09-10 23:41:19 -0500

    added exceptions package containing the Abstract exception class and its layer subclasses

---
+++ bank/src/main/java/com/revature/exceptions/RepositoryException.java
@@ -0,0 +1,11 @@
+package com.revature.exceptions;
+
+public class RepositoryException extends BankException {
+    public RepositoryException(String message) {
+        super(message);
+    }
+
+    public RepositoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
