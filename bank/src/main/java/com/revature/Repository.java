97af7c10325f5c33ec627ce130ba98d812913564
connorg2404 <connortownson@gmail.com>
2026-09-14 13:43:28 -0400

    added repo layer methods

---
+++ bank/src/main/java/com/revature/Repository.java
@@ -0,0 +1,38 @@
+package com.revature;
+
+public class Repository {
+    // Adds a new account after a user registers
+    public static void addAccount() {
+
+    }
+
+
+    public static void getAccount() {
+
+    }
+
+    // Run one time once the user logs in
+    public static void getBalance() {
+
+    }
+
+    // Updates the value of balance during deposits, withdraws, and transfers
+    public static void updateBalance(double amount) {
+
+    }
+
+    // Perform manualCommitConnect() and facilitate a transfer
+    public static void transfer(String accountFrom, String accountTo, double amount) {
+
+    }
+
+    // Adds a transaction associated for an associated accountID
+    public static void addTransaction() {
+
+    }
+
+    // Retrieves the tranactions for an associated accountID
+    public static void viewTransactionHistory() {
+
+    }
+}
