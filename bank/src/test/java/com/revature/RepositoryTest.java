4e08988644eae99cac277513c226904cb88bd141
YousefAbuadas <sparkburner@gmail.com>
2026-09-16 20:11:12 -0500

    added tests

--- bank/src/test/java/com/revature/RepositoryTest.java
+++ bank/src/test/java/com/revature/RepositoryTest.java
@@ -16,4 +16,46 @@ import com.revature.utility.ConnectionFactory;
 import com.revature.utility.MoneyUtils;
 
 public class RepositoryTest {
+    @Test 
+    @DisplayName ("Should return a non-empty list of transactions")
+    void testValidGetTransactionHistory() throws Exception {
+        Connection connection = Mockito.mock(Connection.class);
+        PreparedStatement statement = Mockito.mock(PreparedStatement.class);
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+
+        Mockito.when(connection.prepareStatement(Mockito.anyString())).thenReturn(statement);
+        Mockito.when(statement.executeQuery()).thenReturn(resultSet);
+        Mockito.when(resultSet.next()).thenReturn(true, false);
+        Mockito.when(resultSet.getString("account_id")).thenReturn("Billy");
+        Mockito.when(resultSet.getString("transaction_type")).thenReturn("DEPOSIT");
+        Mockito.when(resultSet.getInt("amount_cents")).thenReturn(1250);
+        Mockito.when(resultSet.getString("related_account_id")).thenReturn("Billy");
+        Mockito.when(resultSet.getString("creationDate")).thenReturn("blaaaablaa");
+        try (MockedStatic<ConnectionFactory> connectionFactory = Mockito.mockStatic(ConnectionFactory.class)) {
+            connectionFactory.when(() -> ConnectionFactory.getAutoCommitConnect()).thenReturn(connection);
+            ArrayList<Transaction> transactions = Repository.getTransactionHistory("Billy");
+            assertEquals(1, transactions.size());
+            Transaction transaction = transactions.get(0);
+            assertEquals("Billy", transaction.getAccountId());
+            assertEquals("DEPOSIT", transaction.getType());
+            assertEquals(new BigDecimal("12.50"), transaction.getAmount());
+            assertEquals("Billy", transaction.getRelatedAccountId());
+            assertEquals("blaaaablaa", transaction.getCreationDate());
+        }
+    }
+
+    @Test 
+    @DisplayName ("Should throw EmptyTransactionHistoryException error")
+    void testInvalidGetTransactionHistory() throws BankException, SQLException {
+        Connection connection = Mockito.mock(Connection.class);
+        PreparedStatement statement = Mockito.mock(PreparedStatement.class);
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.when(connection.prepareStatement(Mockito.anyString())).thenReturn(statement);
+        Mockito.when(statement.executeQuery()).thenReturn(resultSet);
+        Mockito.when(resultSet.next()).thenReturn(false);
+        try (MockedStatic<ConnectionFactory> connectionFactory = Mockito.mockStatic(ConnectionFactory.class)) {
+            connectionFactory.when(ConnectionFactory::getAutoCommitConnect).thenReturn(connection);
+            assertThrows(EmptyTransactionHistoryException.class, () -> Repository.getTransactionHistory("missing-account"));
+        }
+    }
 }
