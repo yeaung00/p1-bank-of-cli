cce3c686c1495abbef562e30594d27f24de07d5e
Damon Lin <74471750+dlin07@users.noreply.github.com>
2026-09-17 13:58:17 -0400

    skeleton code for testing viewBalance

--- bank/src/test/java/com/revature/BusinessTest.java
+++ bank/src/test/java/com/revature/BusinessTest.java
@@ -13,4 +13,18 @@ import com.revature.exceptions.*;
 import com.revature.exceptions.customexceptions.*;
 
 public class BusinessTest {
+
+    // this test entirely depends on the state of your database, I'm currently working on normalizing the test
+    // database to keep everything consistent
+    @Test
+    void testViewBalance() throws BankException {
+        BigDecimal expectedBalance = new BigDecimal(0);
+
+        try (MockedStatic<Repository> repo = Mockito.mockStatic(Repository.class)) {
+            repo.when(() -> Repository.getBalance("Billy")).thenReturn(expectedBalance);
+        }
+
+        BigDecimal actualBalance = Business.viewBalance("Billy");
+        assertEquals(0, actualBalance.compareTo(expectedBalance));
+    }
 }
