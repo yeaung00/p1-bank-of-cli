97b355251dadc1f38be2bc3f7d0d63965c5c9eab
connorg2404 <connortownson@gmail.com>
2026-09-16 20:53:19 -0400

    wrote initial tests for deposit methods

--- bank/src/test/java/com/revature/RepositoryTest.java
+++ bank/src/test/java/com/revature/RepositoryTest.java
@@ -16,4 +16,53 @@ import com.revature.utility.ConnectionFactory;
 import com.revature.utility.MoneyUtils;
 
 public class RepositoryTest {
+
+    @BeforeEach
+    void createDummyInstance() throws SQLException {
+        String sqlQuery = "insert into accounts (account_id, pin_hash, balance_cents) values (?, ?, ?)";
+        try (
+            Connection connection = ConnectionFactory.getAutoCommitConnect();
+            PreparedStatement ps = connection.prepareStatement(sqlQuery);
+        ) {
+            ps.setString(1, "TestUser");
+            ps.setString(2, "Test");
+            ps.setInt(3, 10145);
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new SQLException();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testValidUpdateBalance() throws RepositoryException {
+        int result = Repository.updateBalance("TestUser", new BigDecimal("102.45"));
+        BigDecimal balance = Repository.getBalance("TestUser");
+        Assertions.assertEquals(new BigDecimal("102.45"), balance);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test 
+    void testInvalidUpdateBalance() throws RepositoryException {
+        Assertions.assertThrows(RepositoryException.class, () -> {Repository.updateBalance("UserDoesntExist", new BigDecimal("102.45"));});
+    }
+
+    @AfterEach 
+    void clearDummyInstance() {
+        String sqlQuery = "delete from accounts where account_id = ?";
+        try (
+            Connection connection = ConnectionFactory.getAutoCommitConnect();
+            PreparedStatement ps = connection.prepareStatement(sqlQuery);
+        ) {
+            ps.setString(1, "TestUser");
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new SQLException();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
