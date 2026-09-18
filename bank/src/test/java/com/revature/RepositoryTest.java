e41cdd33f23eb3dc786506bef8073313f7152744
YousefAbuadas <sparkburner@gmail.com>
2026-09-17 21:03:19 -0500

    added tests and new insert statement for dummy db

--- bank/src/test/java/com/revature/RepositoryTest.java
+++ bank/src/test/java/com/revature/RepositoryTest.java
@@ -3,60 +3,14 @@ package com.revature;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
-import java.sql.*;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.*;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 import com.revature.exceptions.*;
 import com.revature.exceptions.customexceptions.*;
-import com.revature.utility.ConnectionFactory;
-import com.revature.utility.MoneyUtils;
 
 public class RepositoryTest {
-    @Test 
-    @DisplayName ("Should return a non-empty list of transactions")
-    void testValidGetTransactionHistory() throws Exception {
-        Connection connection = Mockito.mock(Connection.class);
-        PreparedStatement statement = Mockito.mock(PreparedStatement.class);
-        ResultSet resultSet = Mockito.mock(ResultSet.class);
-
-        Mockito.when(connection.prepareStatement(Mockito.anyString())).thenReturn(statement);
-        Mockito.when(statement.executeQuery()).thenReturn(resultSet);
-        Mockito.when(resultSet.next()).thenReturn(true, false);
-        Mockito.when(resultSet.getString("account_id")).thenReturn("Billy");
-        Mockito.when(resultSet.getString("transaction_type")).thenReturn("DEPOSIT");
-        Mockito.when(resultSet.getInt("amount_cents")).thenReturn(1250);
-        Mockito.when(resultSet.getString("related_account_id")).thenReturn("Billy");
-        Mockito.when(resultSet.getString("creationDate")).thenReturn("blaaaablaa");
-        try (MockedStatic<ConnectionFactory> connectionFactory = Mockito.mockStatic(ConnectionFactory.class)) {
-            connectionFactory.when(() -> ConnectionFactory.getAutoCommitConnect()).thenReturn(connection);
-            ArrayList<Transaction> transactions = Repository.getTransactionHistory("Billy");
-            assertEquals(1, transactions.size());
-            Transaction transaction = transactions.get(0);
-            assertEquals("Billy", transaction.getAccountId());
-            assertEquals("DEPOSIT", transaction.getType());
-            assertEquals(new BigDecimal("12.50"), transaction.getAmount());
-            assertEquals("Billy", transaction.getRelatedAccountId());
-            assertEquals("blaaaablaa", transaction.getCreationDate());
-        }
-    }
-
-    @Test 
-    @DisplayName ("Should throw EmptyTransactionHistoryException error")
-    void testInvalidGetTransactionHistory() throws BankException, SQLException {
-        Connection connection = Mockito.mock(Connection.class);
-        PreparedStatement statement = Mockito.mock(PreparedStatement.class);
-        ResultSet resultSet = Mockito.mock(ResultSet.class);
-        Mockito.when(connection.prepareStatement(Mockito.anyString())).thenReturn(statement);
-        Mockito.when(statement.executeQuery()).thenReturn(resultSet);
-        Mockito.when(resultSet.next()).thenReturn(false);
-        try (MockedStatic<ConnectionFactory> connectionFactory = Mockito.mockStatic(ConnectionFactory.class)) {
-            connectionFactory.when(ConnectionFactory::getAutoCommitConnect).thenReturn(connection);
-            assertThrows(EmptyTransactionHistoryException.class, () -> Repository.getTransactionHistory("missing-account"));
-        }
     @BeforeAll
     static void setupDatabase() throws Exception {
         TestDatabaseHelper.setupDatabase();
@@ -67,6 +21,11 @@ public class RepositoryTest {
         TestDatabaseHelper.populateDatabase();
     }
 
+    @AfterEach
+    void clearDatabase() throws Exception {
+        TestDatabaseHelper.clearDatabase();
+    }
+
     @Test
     void testGetBalance() throws AccountNotFoundException, DatabaseException {
         BigDecimal balance = Repository.getBalance("Billy");
@@ -74,8 +33,27 @@ public class RepositoryTest {
         assertEquals(0, balance.compareTo(new BigDecimal("1.00")));
     }
 
-    @AfterEach
-    void clearDatabase() throws Exception {
-        TestDatabaseHelper.clearDatabase();
+    @Test 
+    @DisplayName ("Should return a non-empty list of transactions")
+    void testValidGetTransactionHistory() throws Exception {
+        ArrayList<Transaction> transactions = Repository.getTransactionHistory("Billy");
+
+        assertEquals(2, transactions.size());
+        assertEquals("Billy", transactions.get(0).getAccountId());
+        assertEquals("DEPOSIT", transactions.get(0).getType());
+        assertEquals(new BigDecimal("1.00"), transactions.get(0).getAmount());
+        assertEquals("Billy", transactions.get(0).getRelatedAccountId());
+        assertEquals("TRANSFER_OUT", transactions.get(1).getType());
+        assertEquals(new BigDecimal("0.25"), transactions.get(1).getAmount());
+        assertEquals("Sally", transactions.get(1).getRelatedAccountId());
+    }
+
+    @Test 
+    @DisplayName ("Should throw EmptyTransactionHistoryException error")
+    void testInvalidGetTransactionHistory() throws BankException {
+        assertThrows(
+                EmptyTransactionHistoryException.class,
+                () -> Repository.getTransactionHistory("missing-account")
+        );
     }
 }
