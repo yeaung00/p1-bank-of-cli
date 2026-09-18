20891f15128a6e89befd786120eda023c8f4acb5
connorg2404 <connortownson@gmail.com>
2026-09-17 21:29:05 -0400

    added repo testing methods

--- bank/src/test/java/com/revature/RepositoryTest.java
+++ bank/src/test/java/com/revature/RepositoryTest.java
@@ -33,6 +33,19 @@ public class RepositoryTest {
         assertEquals(0, balance.compareTo(new BigDecimal("1.00")));
     }
 
+    @Test
+    void testValidUpdateBalance() throws RepositoryException {
+        int result = Repository.updateBalance("Billy", new BigDecimal("102.45"));
+        BigDecimal balance = Repository.getBalance("Billy");
+        Assertions.assertEquals(1, result);
+        Assertions.assertEquals(new BigDecimal("102.45"), balance);
+    }
+
+    @Test 
+    void testInvalidUpdateBalance() throws RepositoryException {
+        Assertions.assertThrows(RepositoryException.class, () -> {Repository.updateBalance("UserDoesntExist", new BigDecimal("102.45"));});
+    }
+
     @AfterEach
     void clearDatabase() throws Exception {
         TestDatabaseHelper.clearDatabase();
